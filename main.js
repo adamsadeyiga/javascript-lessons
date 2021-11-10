6897dffbfdae83821c33d6e32e658395f59c5867
Adams Adeyiga <adamsadeyiga1@gmail.com>
2021-11-10 22:51:46 +0100

    init commit

---
+++ main.js
@@ -0,0 +1,28 @@
+// let text = "adams jamiu is a good man";
+// let result = text.length;
+// console.log(result);
+
+// console.log(text.length);
+// console.log(text.toLowerCase());
+// console.log(text.toUpperCase());
+// console.log(text.charAt(0));
+// console.log(text.charAt(text.length - 1));
+// console.log(text.indexOf("a"));
+// console.log(text.trim());
+// console.log(text.startsWith("adams"));
+// console.log(text.slice(0, text.indexOf("s") + 1));
+
+// let names = ["adams", "jamiu", "adeyiga", "akeeb", "alarape"];
+
+// console.log(names.length);
+// console.log(names[names.indexOf("alarape")]);
+
+// let person = { name: "adams" };
+// let person2 = { ...person, name: "alarape" };
+
+// console.log(person.name);
+// console.log(person2.name);
+
+// console.log(document.body);
+
+console.dir(document);
